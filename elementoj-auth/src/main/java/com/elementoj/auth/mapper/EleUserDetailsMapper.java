e7f8d7384f16c45bc326a0c47c32e4b35f510745
qiyuan11111 <1746257500@qq.com>
2025-07-23 23:49:05 +0800

    修改@ResultMap由注解变为xml

--- elementoj-auth/src/main/java/com/elementoj/auth/mapper/EleUserDetailsMapper.java
+++ elementoj-auth/src/main/java/com/elementoj/auth/mapper/EleUserDetailsMapper.java
@@ -2,8 +2,12 @@ package com.elementoj.auth.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.elementoj.auth.domain.EleUserDetails;
-import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.*;
 
 public interface EleUserDetailsMapper extends BaseMapper<EleUserDetailsMapper> {
+    @ResultMap("EleUserDetailsMap")
+    @Select({"select * " +
+             "from (select user_name, password, user_id from user where user_name = #{user_name}) as a " +
+             "  inner join (select authority from authority where user_name = #{user_name}) as b"})
     EleUserDetails selectUserDetailsByUserName(@Param("user_name") String userName);
 }
--- elementoj-auth/src/main/java/com/elementoj/auth/mapper/EleUserMapper.java
+++ elementoj-auth/src/main/java/com/elementoj/auth/mapper/EleUserMapper.java
@@ -8,12 +8,7 @@ import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 public interface EleUserMapper extends BaseMapper<EleUser> {
-    @Results(id = "eleUser",
-    value = {
-            @Result(property = "userId", column = "user_id", id = true),
-            @Result(property = "userName", column = "user_name")
-    })
-    @ResultMap("eleUser")
+    @ResultMap("EleUserMap")
     @Select({"select userId, userName from user where user_id = #{uid}"})
     EleUser getUserById(Long uid);
 }
