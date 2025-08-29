f8ef7297e06d51ad647d7827ee1b78d778d70690
zhengyiming14 <zhengyiming14@jd.com>
2025-08-29 17:39:38 +0800

    gateway

--- elementoj-module/elementoj-module-news/src/main/java/com/elementoj/module/news/ModuleNewsApplication.java
+++ elementoj-module/elementoj-module-news/src/main/java/com/elementoj/module/news/ModuleNewsApplication.java
@@ -4,12 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScans({
         @MapperScan("com.elementoj.**.mapper"),
         @MapperScan("com.elementoj.**.mapbean")
 })
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ModuleNewsApplication {
     public static void main(String[] args) {
         SpringApplication.run(ModuleNewsApplication.class, args);
